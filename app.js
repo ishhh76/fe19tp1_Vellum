53d578df3690545ad0969d173455ae6fa91640c0
Danilo <danilo.buttafoco@studet.kyh.se>
2019-11-08 11:14:39 +0100

    latest update

--- app.js
+++ app.js
@@ -2,38 +2,102 @@
 
 function openNav() {
     document.getElementById("secondSideBar").style.width = "300px";
-    document.getElementById("main").style.width = "70%";
-    document.getElementById("main").style.marginLeft = "420px";
+    document.getElementById("editor").style.width = "70%";
+    document.getElementById("editor").style.marginLeft = "420px";
 }
 
 function closeNav() {
     document.getElementById("secondSideBar").style.width = "0";
-    document.getElementById("main").style.width = "90%";
-    document.getElementById("main").style.marginLeft = "130px";
+    document.getElementById("editor").style.width = "90%";
+    document.getElementById("editor").style.marginLeft = "130px";
 }
 
-tinymce.init({
-    height: "800px",
-    selector: 'textarea',  // change this value according to the HTML
-    plugins: ['print', 'image imagetools', 'wordcount', 'save', 'lists', 'fullscreen', 'bbcode'],
-    menubar: false,
-    toolbar: [
-        'newdocument print | undo redo | numlist bullist | italic bold | alignleft aligncenter alignright | styleselect | wordcount link image fullscreen save'
-    ],
-    style_formats: [
-        { title: 'Heading 1', format: 'h1'},
-        { title: 'Heading 2', format: 'h2' },
-        { title: 'Heading 3', format: 'h3' },
-        { title: 'Heading 4', format: 'h4' },
-        { title: 'Heading 5', format: 'h5' },
-        { title: 'Paragraph', format: 'p' }
-      ],
+/* Editor */
 
-      save_onsavecallback: function () {
-          var content = tinymce.activeEditor.getContent();
-          console.log(content);
-          localStorage.setItem('name', content);
-          console.log(localStorage);
-      }
-  });
+const savebtn = document.querySelector('.savebtn');
+const justText = document.querySelector('#justText');
+var Delta = Quill.import('delta');
+
+var options = {
+  modules: {
+    toolbar: [
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline'],
+    [ 'link' ,'image'],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'align': [] }],
+  ]
+  },
+    placeholder: null,
+    // 'Write something fun...'
+    theme: 'snow'
+  };
+  
+  var editor = new Quill('#quillEditor', options);
+  var justTextContent = document.getElementById('justText');
+  
+  // editor.on('text-change', function() {
+  //   var text = editor.root.innerHTML;
+  //   justTextContent.innerHTML = text;
+  // });
+
+  var change = new Delta();
+editor.on('text-change', function(delta) {
+ change = change.compose(delta);
+});
+// Save periodically
+setInterval(function() {
+ if (change.length() > 0) {
+   // Save the entire updated text to localStorage
+   const data = JSON.stringify(editor.getContents());
+   localStorage.setItem('key', data);
+   change = new Delta();
+ };
+}, 5*1000);
+
+  const retrieveData = () => {
+    const loadData = JSON.parse(localStorage.getItem('key'));
+    editor.setContents(loadData);
+   }
+   savebtn.addEventListener('click', ()=> {
+    let newDiv = document.createElement("div");
+    justText.appendChild(newDiv);
+    newDiv.classList.add('div');
+    let NewData = editor.root.innerHTML;
+    newDiv.innerHTML = NewData;
+   });
+   justText.addEventListener('click', e => {
+    if(e.target) {
+      retrieveData();
+    }else {console.log('hej')}
+   })
+
+  /* Popup */
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
   
